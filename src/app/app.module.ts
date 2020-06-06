17bafa0f917627e9199c86cb54818fc552655fea
anderson <anderdavid86@hotmail.com>
2020-06-06 11:07:37 -0500

    Se crea componente de prueba y barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { NavbarComponent } from './navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test-component/test-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component',
+  templateUrl: './test-component.component.html',
+  styleUrls: ['./test-component.component.css']
+})
+export class TestComponentComponent implements OnInit {
+
+  title ="mi componente de prueba"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
