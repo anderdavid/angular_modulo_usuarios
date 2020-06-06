f89e681a41022c70fc23153f95b6a93c108d3c45
anderson <anderdavid86@hotmail.com>
2020-06-06 17:46:25 -0500

    Se integra framework css foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component,OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import 'foundation-sites';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angModUser';
+  constructor(){
+    //$(document).foundation()
+  }
   ngOnInit(): void {
     console.log('ngInit()')
+    $(document).foundation()
+    /* 
     $(document).ready(() => {
       console.log('The document ready!')
-    });
+      
+    }); */
+   
   }
 }
