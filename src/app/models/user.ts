875e0eac0006ec7a406d47f678298487deb2895f
anderson <anderdavid86@hotmail.com>
2020-06-06 20:37:18 -0500

    Se crea modelo user

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User{
+    constructor(
+        public id:string,
+        public nombre:string,
+        public apellido:string,
+        public cedula:string,
+        public edad:string,
+        public email:string,
+        public password:string
+    ){}
+}
